a94090ce7698414718144687a282768a563a173f
ibrohim <yvycvydfgg@gmail.com>
2023-11-11 15:07:56 +0500

    init

---
+++ src/main/java/com/example/onlinemedicine/OnlineMedicineApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinemedicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMedicineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineMedicineApplication.class, args);
+    }
+
+}
