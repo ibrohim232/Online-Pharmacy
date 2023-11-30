02659a5a8d479df5ccb5e131bad4c7f3a5296880
ibrohim <yvycvydfgg@gmail.com>
2023-11-30 11:20:27 +0500

    cros and me api

--- src/main/java/com/example/onlinemedicine/config/CorsConfig.java
+++ src/main/java/com/example/onlinemedicine/config/CorsConfig.java
@@ -15,10 +15,9 @@ public class CorsConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
             }
         };
     }
