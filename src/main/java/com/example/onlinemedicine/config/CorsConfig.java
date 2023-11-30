f6a81cf2534c720d0c66248eb351c72835276533
ibrohim <yvycvydfgg@gmail.com>
2023-11-30 14:55:11 +0500

    cros added

--- src/main/java/com/example/onlinemedicine/config/CorsConfig.java
+++ src/main/java/com/example/onlinemedicine/config/CorsConfig.java
@@ -1,25 +1,39 @@
 package com.example.onlinemedicine.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
-            }
-        };
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Allow all origins, you should customize this based on your security requirements
+        config.addAllowedOrigin("*");
+
+        // Allow specific HTTP methods (GET, POST, PUT, DELETE, etc.)
+        config.addAllowedMethod("*");
+
+        // Allow specific headers (e.g., Content-Type, Authorization)
+        config.addAllowedHeader("*");
+
+        // Allow credentials (e.g., cookies, authentication)
+        config.setAllowCredentials(true);
+
+        // Set the maximum age (in seconds) of the preflight request
+        config.setMaxAge(3600L);
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
+
 }
 
