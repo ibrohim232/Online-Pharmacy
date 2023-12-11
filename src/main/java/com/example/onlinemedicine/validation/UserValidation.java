5b4f817714c2b02dc69b80f683244270d6be5bde
ACER <bakhromdev@gmail.com>
2023-12-11 18:27:44 +0500

    phone number validation changed

--- src/main/java/com/example/onlinemedicine/validation/UserValidation.java
+++ src/main/java/com/example/onlinemedicine/validation/UserValidation.java
@@ -23,7 +23,7 @@ public class UserValidation {
         if (user.isPresent()) {
             return false;
         }
-        return Pattern.matches("^//+998((0-9){2}|[0-9]{2})[0-9]{7}$", phoneNumber);
+        return Pattern.matches("^\\+998((0-9){2}|[0-9]{2})[0-9]{7}$", phoneNumber);
     }
 
     public boolean isValidPassword(String password) {
