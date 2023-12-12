2f047fab97f87edb0b6733b572e2d2eb943535cd
ACER <bakhromdev@gmail.com>
2023-12-12 21:39:42 +0500

    swagger config changed

--- src/main/java/com/example/onlinemedicine/config/BeanConfig.java
+++ src/main/java/com/example/onlinemedicine/config/BeanConfig.java
@@ -30,23 +30,23 @@ public class BeanConfig {
         return new BCryptPasswordEncoder();
     }
 
-    private SecurityScheme createAPIKeyScheme() {
-        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
-                .bearerFormat("JWT")
-                .scheme("bearer");
-    }
-    @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI().addSecurityItem(new SecurityRequirement().
-                        addList("Bearer Authentication"))
-                .components(new Components().addSecuritySchemes
-                        ("Bearer Authentication", createAPIKeyScheme()))
-                .info(new Info().title("My REST API")
-                        .description("Some custom description of API.")
-                        .version("1.0").contact(new Contact().name("Sallo Szrajbman")
-                                .email( "www.baeldung.com").url("salloszraj@gmail.com"))
-                        .license(new License().name("License of API")
-                                .url("API license URL")));
-    }
+//    private SecurityScheme createAPIKeyScheme() {
+//        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+//                .bearerFormat("JWT")
+//                .scheme("bearer");
+//    }
+//    @Bean
+//    public OpenAPI openAPI() {
+//        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+//                        addList("Bearer Authentication"))
+//                .components(new Components().addSecuritySchemes
+//                        ("Bearer Authentication", createAPIKeyScheme()))
+//                .info(new Info().title("My REST API")
+//                        .description("Some custom description of API.")
+//                        .version("1.0").contact(new Contact().name("Sallo Szrajbman")
+//                                .email( "www.baeldung.com").url("salloszraj@gmail.com"))
+//                        .license(new License().name("License of API")
+//                                .url("API license URL")));
+//    }
 
 }
