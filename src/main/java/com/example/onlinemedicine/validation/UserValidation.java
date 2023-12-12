23a56480d3a16c8d0a28c9788e8c7fe2414efd7f
ibrohim <yvycvydfgg@gmail.com>
2023-12-12 16:18:07 +0500

    photo

--- src/main/java/com/example/onlinemedicine/validation/UserValidation.java
+++ src/main/java/com/example/onlinemedicine/validation/UserValidation.java
@@ -23,7 +23,6 @@ public class UserValidation {
         if (user.isPresent()) {
             return false;
         }
-        return true;
         return Pattern.matches("^\\+998((0-9){2}|[0-9]{2})[0-9]{7}$", phoneNumber);
     }
 
